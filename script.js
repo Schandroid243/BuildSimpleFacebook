c5320993b9a311eb991973b134244c9381fdc62f
Schandroid243 <schadrackngunza@gmail.com>
2021-09-02 13:50:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var database = [
+    {
+        username: "Schandroid243",
+        password: "12345"
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning !"
+    },
+    {
+        username: "Sally",
+        timeline: "JavaScript is soooo cool !"
+    },
+    {
+        username: "Mitch",
+        timeline: "JavaScript is preetyy coooooooooolll !!!"
+    },
+];
